dfd3a427ac5f113a15e958a2d5a5dff799fe39b9
Aidar <c>
2020-10-12 17:03:39 +0300

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import find_packages, setup
 
-VERSION = 0.1
+VERSION = "0.1.1"
 
 with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
     README = readme.read()
